4ed219e804c664daa10f8f0844aa0f3d2e0f5343
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2019-09-02 18:49:42 -0700

    Can select Sub. Back button next

--- boot.py
+++ boot.py
@@ -5,57 +5,62 @@
 #import webrepl
 #webrepl.start()        
 
-# def get_button_state():
-#         import button_listener
-#         active_pin = button_listener.checkPinState()
-#         # print(active_pin)
-#         return active_pin
-
-
 def button():
         import time
         from machine import Pin
         from button_listener import ButtonListener
         from menu_gui import Menu
+        from menus import MenuOptions
 
         menu = Menu()
+        mo = MenuOptions()
         bl = ButtonListener()
         moved = False
         row = 0
         y_values = [1,11,21,31,41,51]
-        # menu_options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6']
-        
 
-        menu.drawBorder()
         led = Pin(27, Pin.OUT, Pin.PULL_DOWN)
         led.value(1)
-        menu.refresh()
 
+        
+        # menu.drawBorder()
+        menu.setMenuList(mo.mainMenu)
+        currentMenu = menu.menu_options
+        menu.initMenuOptions(currentMenu)
+        menu.refresh()
 
         while True:
                 led_status = led.value()
                 active_butt = bl.checkPinState()
-                # menu.display.rect(0 , 0, 128, 64, 1)
                 if active_butt != 'none' and moved == False:
-                        # menu.display.rect(0 , 0, 128, 64, 1)
                         menu.drawSelect(y_values[row], 1)
-                        menu.renderOptions(row)
+                        menu.renderOptions(row, currentMenu)
                         menu.refresh()
                         moved = True
 
-                if row < 5 and (active_butt == 'down' and moved == True):
+                if row < len(menu.menu_options)-1 and (active_butt == 'down' and moved == True):
                         row += 1
-                        # menu.display.rect(0 , 0, 128, 64, 1)
                         menu.drawSelect(y_values[row], 1)
-                        menu.renderOptions(row)
+                        menu.renderOptions(row, currentMenu)
                         menu.refresh()
+                        print(row)
 
                 if row > 0 and (active_butt == 'up' and moved == True):
                         row -= 1
-                        # menu.display.rect(0 , 0, 128, 64, 1)
                         menu.drawSelect(y_values[row], 1)
-                        menu.renderOptions(row)
+                        menu.renderOptions(row, currentMenu)
                         menu.refresh()
+                        print(row)
+
+                if active_butt == 'center':
+                        print(menu.menu_options[row].get('action'))
+                        menu.setMenuList(menu.menu_options[row].get('action'))
+                        currentMenu = menu.menu_options
+                        menu.initMenuOptions(currentMenu)
+                        row = 0
+                        moved = False
+                        menu.refresh()
+                        time.sleep(.5)
 
                 if active_butt == 'side_b' and led_status == 1:
                         led.value(0)
@@ -64,6 +69,5 @@ def button():
                         led.value(1)
                         time.sleep(.25)
                         
-                # display.text('{}'.format(active_butt), 2, 2, 1)
                 time.sleep(.1)
 
--- menu_gui.py
+++ menu_gui.py
@@ -1,19 +1,26 @@
 import sh1106
 from machine import Pin, I2C
+from menus import MenuOptions
 
 class Menu():
     def __init__(self):
+        self.mo = MenuOptions()
         self.i2c = I2C(scl=Pin(16), sda=Pin(17), freq=400000)
         self.display = sh1106.SH1106_I2C(128, 64, self.i2c, Pin(16), 0x3c)
         self.display.sleep(False)
-        # self.display.fill(0)
         self.display.rotate(True,update=True)
-        # self.display.rect(0 , 0, 128, 64, 1)
         self.refresh()
         self.y_values = [1,11,21,31,41,51]
-        self.menu_options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6']
-        for option in self.menu_options:
-            self.renderText(option, self.y_values[self.menu_options.index(option)]+2, 1)
+        # self.menu_options = self.mo.getMenuOpts(self.mo.mainMenu)
+        self.menu_options = list()
+        # for option in self.menu_options:
+        #     self.renderText(option.get('name'), self.y_values[self.menu_options.index(option)]+2, 1)
+
+    def setMenuList(self, menuName):
+        self.menu_options = self.mo.getMenuOpts(menuName)
+
+    def getMenuList(self, menuName):
+        return self.mo.getMenuOpts(menuName)
 
     def drawBorder(self):
         self.display.rect(0 , 0, 128, 64, 1)
@@ -30,11 +37,13 @@ class Menu():
     def renderText(self, text, y_pos, color):
         self.display.text(text, 2, y_pos, color)
 
-    def renderOptions(self, row):
-        for option in self.menu_options:
-            if self.menu_options.index(option) == row:
-                self.renderText(option, self.y_values[self.menu_options.index(option)]+2, 0)
-            else:
-                self.renderText(option, self.y_values[self.menu_options.index(option)]+2, 1)
+    def initMenuOptions(self, menuName):
+        for option in menuName:
+            self.renderText(option.get('name'), self.y_values[menuName.index(option)]+2, 1)
 
-        # self.refresh()
+    def renderOptions(self, row, options):
+        for option in options:
+            if options.index(option) == row:
+                self.renderText(option.get('name'), self.y_values[options.index(option)]+2, 0)
+            else:
+                self.renderText(option.get('name'), self.y_values[options.index(option)]+2, 1)
